1296762ec49a6f4256facf7bd533020a97c29622
Jiguo Zhang <75199165@qq.com>
2016-06-23 11:44:54 +0800

    fix

--- tupu/ssd_detector.py
+++ tupu/ssd_detector.py
@@ -3,8 +3,7 @@ import os
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../python'))
 import cv2
 import caffe
-import numpy
-from utils.timer import Timer
+import numpy as np
 
 def gpu_init(id):
     caffe.set_mode_gpu()
@@ -17,18 +16,16 @@ class SSDDetector(object):
         self.mean = np.array([104, 117, 123], np.uint8)
         self.net = caffe.Detector(prototxt, caffemodel,mean=self.mean)
 
-    def detect(self, im_data):
+    def detect(self, im):
     	h = im.shape[0]
     	w = im.shape[1]
     	r_im = cv2.resize(im, (300, 300))
     	blob = np.zeros((1, 3, 300,300), np.float32)
-    
-    	in_ = net.inputs[0]
-    	pre_time = time.time()
-    	blob[0] = net.transformer.preprocess(in_, r_im)
+    	in_ = self.net.inputs[0]
+    	blob[0] = self.net.transformer.preprocess(in_, r_im)
     	forward_kwargs = {in_: blob}
-    	blobs_out = net.forward(**forward_kwargs)
-    	res = blobs_out[net.outputs[0]] 
+    	blobs_out = self.net.forward(**forward_kwargs)
+    	res = blobs_out[self.net.outputs[0]]
         dets = {}
     	for i in range(res.shape[2]):
             box = res[0,0,i]
@@ -36,17 +33,19 @@ class SSDDetector(object):
             score = box[2]
             if score < 0.1:
                 continue
-            xmin = int(box[3]*w)
-            ymin = int(box[4]*h)
-            xmax = int(box[5]*w)
-            ymax = int(box[6]*h)
+            xmin = float(int(box[3]*w))
+            ymin = float(int(box[4]*h))
+            xmax = float(int(box[5]*w))
+            ymax = float(int(box[6]*h))
             det = [xmin, ymin, xmax, ymax, score]
             if label not in dets:
                 dets[label] = []
             dets[label].append(det)
-        return dets
-       
-     def draw_detection_result(self, im_data, class_name, dets):
+        np_dets = {}
+        for k, v in dets.items():
+            np_dets[k] = np.array(v, np.float32)
+        return np_dets
+    def draw_detection_result(self, im_data, class_name, dets):
         font = cv2.FONT_HERSHEY_SIMPLEX
         for i in range(dets.shape[0]):
             bbox = dets[i, :4]
